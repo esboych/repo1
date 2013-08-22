8ca66516d140106cbb6ecb0a99544ac52b962dab
esboychakov <esboychakov@esboychakov-ws>
2013-08-22 13:27:07 +0300

    all sensor data is presented on web GUI

--- Spring_MVC_2_process_request_params/src/main/java/my2/web/com/HomeController.java
+++ Spring_MVC_2_process_request_params/src/main/java/my2/web/com/HomeController.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,10 +25,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 	
-	static int sense2intDec;
-	static int sensorSampleCount;
+	private static int sense0intDec, sense1intDec, sense2intDec, sense3intDec, sense4intDec, sense5intDec, sense6intDec;
+	private static int sensorSampleCount;
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	static String formattedDate;
+	private static String formattedDate;
+	private static boolean isUpdated;
 
 	@RequestMapping(value = "/local")
 	public String webHome(Locale locale, Model model) {
@@ -39,34 +41,62 @@ public class HomeController {
 	@RequestMapping(value = "/SSE")
 	public @ResponseBody void noKeys(Locale locale, Model model, HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		System.out.println("noKeys is called");
+
 		while(true){
-			Thread.sleep(500);
+			Thread.sleep(1200);
 			
 			Date date = new Date();
 			DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);		
 			formattedDate = dateFormat.format(date);
-			
+			isUpdated=false;
 			resp.setContentType("text/event-stream");
 			resp.setCharacterEncoding("UTF-8");  
 			PrintWriter out = resp.getWriter();
-			String data = "data:" + sense2intDec + " uV , count= " + sensorSampleCount + " ,time is " + formattedDate + "\n\n";
+			Random random = new Random();
+//			String data = "data:" + sense2intDec + " uV , count= " + sensorSampleCount + " ,time is " + formattedDate + "\n\n";
+//			String data = "data:" + sense2intDec + "\n\n";
+			String data = "data:{" + 
+					    "\"count\":" + sensorSampleCount +"," +
+					    "\"sens0\":" + sense0intDec +"," +
+					    "\"sens1\":" + sense1intDec +"," +
+					    "\"sens2\":" + sense2intDec +"," +
+					    "\"sens3\":" + sense3intDec +"," +
+					    "\"sens4\":" + sense4intDec +"," +
+					    "\"sens5\":" + sense5intDec +"," +
+						"\"sens6\":" + sense6intDec  +
+						"}\n\n";
 			out.write(data);  
 			out.flush();
+
 		}
 	}
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(@RequestParam ("value") String value, @RequestParam ("id") String id, Locale locale, Model model) {
+	public String home(@RequestParam ("value") String value, Locale locale, Model model) {
 		System.out.println("home is called");
 		sensorSampleCount++;
-
+		System.out.println("form home: isUpdated is true");
 		model.addAttribute("serverTime", formattedDate );
 		System.out.println("server time " + formattedDate);
 		System.out.println("server acccepted value=" + value);
-		System.out.println("server acccepted id=" + id);
-		String sense2str = value.substring(12, 16) + "0";
-		System.out.println("sensor2 in HEX=" + sense2str);
+//		System.out.println("server acccepted id=" + id);
+		String sense0str = value.substring(4, 8) + "0";
+		String sense1str = value.substring(8, 12) + "0";
+		String sense2str = value.substring(12, 16) + "0";  //initial
+		String sense3str = value.substring(16, 20) + "0";
+		String sense4str = value.substring(20, 24) + "0";
+		String sense5str = value.substring(24, 28) + "0";
+		String sense6str = value.substring(28, 32) + "0";	
+//		sense0intDec = 100;
+//		sense1intDec = 2000;
+//		System.out.println("sensor2 in HEX=" + sense2str);
+		sense0intDec = Integer.parseInt(sense0str,16);
+		sense1intDec = Integer.parseInt(sense1str,16);
 		sense2intDec = Integer.parseInt(sense2str,16);
+		sense3intDec = Integer.parseInt(sense3str,16);
+		sense4intDec = Integer.parseInt(sense4str,16);
+		sense5intDec = Integer.parseInt(sense5str,16);
+		sense6intDec = Integer.parseInt(sense6str,16);
 		System.out.println("sensor2 in mV=" + sense2intDec/1000 + " mV"); //per 100 because the last 4 bites are truncated from wifly
 		return "home";
 	}
--- Spring_MVC_2_process_request_params/src/main/webapp/WEB-INF/views/home.jsp
+++ Spring_MVC_2_process_request_params/src/main/webapp/WEB-INF/views/home.jsp
@@ -3,19 +3,98 @@
 <html>
 <head>
 	<title>Home</title>
+	<style type="text/css">
+	#chart{
+		height:450px;
+		width:3200px;
+		top:300px;
+		left:5px;
+		background:	#eee;
+		position:absolute;
+	}
+	.dot
+	{
+		height:5px;
+		width:1px;
+		top:400px;
+		left:50px;
+		background:	#3d3;
+		position:absolute;
+	}
+	</style>
 </head>
 <body>
 <h1>
-	Hello world!  
+	Sensor data 
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
 <div id = "result"></div>
+<div id = "count">count</div>
+<div id = "sens0">sens0</div>
+<div id = "sens1">sens1</div>
+<div id = "sens2">sens2</div>
+<div id = "sens3">sens3</div>
+<div id = "sens4">sens4</div>
+<div id = "sens5">sens5</div>
+<div id = "sens6">sens6</div>
+<div id = "chart"></div>
+
 <script>
+var dotCoordinate = 5;
+var oldValue=20;
+var sens0, sens1;
+function createDot(xBias)
+	{
+		var dot = document.createElement('div');
+		dot.className = 'dot';
+		dotCoordinate++;
+		dot.style.left=dotCoordinate;
+		dot.style.top=750 - (xBias);
+		document.body.appendChild(dot);
+	}	
+		
+</script>
+
+
+<script>
+ createDot(150);
+ createDot(170);
+ createDot(180);
  var source = new EventSource("SSE");
- source.onmessage = function(event){
-	 document.getElementById("result").innerHTML = "sensor data " + event.data;
+/* 
+ source.onmessage = function(event){  //without JSON
+//	 document.getElementById("result").innerHTML = "sensor data " + event.data;
+	 var dataInt = parseInt(event.data);
+	 dataInt=(dataInt-462000)/5;
+	 if(dataInt<0) dataInt=100;
+	 if(dataInt>oldValue) oldValue++;
+	 if(dataInt<oldValue) oldValue--;
+	 document.getElementById("result").innerHTML = "sensor data " + event.data + " uV" + " dataInt= " + dataInt + " oldValue=" + oldValue;
+	 createDot(oldValue);
  }
+ */ 
+  source.onmessage = function(event){  
+	 var data = JSON.parse(event.data);
+	 document.getElementById("result").innerHTML = "sensor data " + data;
+	 sens0 = parseInt(data.sens0);
+	 sens1 = parseInt(data.sens1);
+	 sens2 = parseInt(data.sens2);
+	 sens3 = parseInt(data.sens3);
+	 sens4 = parseInt(data.sens4);
+	 sens5 = parseInt(data.sens5);
+	 sens6 = parseInt(data.sens6);
+	 document.getElementById("count").innerHTML = "count= " + data.count;
+	 document.getElementById("sens0").innerHTML = "sens0= " + sens0;
+	 document.getElementById("sens1").innerHTML = "sens1= " + sens1;
+	 document.getElementById("sens2").innerHTML = "sens2= " + sens2;
+	 document.getElementById("sens3").innerHTML = "sens3= " + sens3;
+	 document.getElementById("sens4").innerHTML = "sens4= " + sens4;
+	 document.getElementById("sens5").innerHTML = "sens5= " + sens5;
+	 document.getElementById("sens6").innerHTML = "sens6= " + sens6;
+	 createDot(oldValue);
+ }
+
 
 </script>
 </body>
